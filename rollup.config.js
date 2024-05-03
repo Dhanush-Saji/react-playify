803e5d8dc5fbe214e4fa998f57fcd7fb82c5c644
Dhanush-Saji <dhanushsaji1@gmail.com>
2024-05-03 15:51:58 +0530

    03-05

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "rollup";
 import typescript from "@rollup/plugin-typescript";
 import nodeResolve from '@rollup/plugin-node-resolve';
+import css from 'rollup-plugin-css-only';
 
 export default defineConfig({
   input: "src/index.ts",
@@ -10,5 +11,5 @@ export default defineConfig({
     name: "react-media-playback",
   },
   external: ["react", "react-dom", "react-icons"],
-  plugins: [typescript({ tsconfig: "tsconfig.json" }),nodeResolve()],
+  plugins: [typescript({ tsconfig: "tsconfig.json" }),nodeResolve(),css({ output: 'src/components/Player/player.css' })],
 });
--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -4,10 +4,10 @@ import Control from '../Controls/Control'
 import Seeker from '../Seeker/Seeker'
 import VolumeBar from '../VolumeBar/VolumeBar'
 import { IoClose } from 'react-icons/io5'
-
+import './Player.css'
 const Player = () => {
   return (
-    <div style={{background:'rgba(0,0,0, 0.5)',boxShadow:'0 4px 30px rgba(0, 0, 0, 0.1)',backdropFilter:'blur(5px)',WebkitBackdropFilter:'blur(5px)',zIndex:9999,position:'absolute',height:'5rem',bottom:0,left:0,right:0,borderRadius:'1.5rem 1.5rem 0 0',display:'flex'}}>
+    <div className='music-player'>
     <div style={{display:'flex',gap:'0.5rem',padding:'1rem',flexDirection:'row',position:'relative',width:'100%',alignItems:'center'}}>
         <Track />
         <div style={{alignItems:'center',display:'flex',flexDirection:'column',justifyContent:'center',margin:'auto'}}>
